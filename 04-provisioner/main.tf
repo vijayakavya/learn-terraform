db2cb4c006e70be152fab9b6cf238b0f50751bdb
kavya <vijayakavya1226@gmail.com>
2023-09-27 16:30:51 +0530

    expense shell

--- 04-provisioner/main.tf
+++ 04-provisioner/main.tf
@@ -3,3 +3,13 @@ resource "null_resource" "test" {
     command = "echo Hello World"
   }
 }
+
+resource "null_resource" "test1" {
+  provisioner "local-exec" {
+    command = <<EOF
+echo Hello World
+pwd
+ls
+EOF
+  }
+}
