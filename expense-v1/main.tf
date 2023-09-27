5b69b04f5d5f8e29cbc16fdf6a591c0946a46b1d
kavya <vijayakavya1226@gmail.com>
2023-09-27 15:34:16 +0530

    expense shell

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,53 +1,53 @@
-#resource "aws_instance" "frontend" {
-#  ami           = data.aws_ami.ami.image_id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
-#
-#  tags = {
-#    Name = "frontend"
-#  }
-#}
-#
-#resource "aws_route53_record" "www" {
-#  zone_id = "Z03665552INK97CUQ6WQD"
-#  name    = "frontend.kdevopsb26.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [ aws_instance.frontend.private_ip ]
-#}
-#
-#resource "aws_instance" "backend" {
-#  ami           = data.aws_ami.ami.image_id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
-#
-#  tags = {
-#    Name = "backend"
-#  }
-#}
-#
-#resource "aws_route53_record" "www" {
-#  zone_id = "Z03665552INK97CUQ6WQD"
-#  name    = "backend.kdevopsb26.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [ aws_instance.backend.private_ip ]
-#}
-#
-#resource "aws_instance" "mysql" {
-#  ami           = data.aws_ami.ami.image_id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
-#
-#  tags = {
-#    Name = "mysql"
-#  }
-#}
-#
-#resource "aws_route53_record" "www" {
-#  zone_id = "Z03665552INK97CUQ6WQD"
-#  name    = "mysql.kdevopsb26.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [ aws_instance.mysql.private_ip ]
-#}
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z03665552INK97CUQ6WQD"
+  name    = "frontend.kdevopsb26.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.frontend.private_ip ]
+}
+
+resource "aws_instance" "backend" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z03665552INK97CUQ6WQD"
+  name    = "backend.kdevopsb26.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.backend.private_ip ]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0bde49699ad6e2424"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z03665552INK97CUQ6WQD"
+  name    = "mysql.kdevopsb26.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.mysql.private_ip ]
+}
