a22040f44165ebb03994baafd7582488f9b586d8
kavya <vijayakavya1226@gmail.com>
2023-09-27 06:56:41 +0530

    expense shell

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z03665552INK97CUQ6WQD"
+  name    = "test.kdevopsb26.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.web.private_ip ]
+}
